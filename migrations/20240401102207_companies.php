e4afc3cede723352b05495f0eee2377deb056fe4
Chris Burton <csburton@gmail.com>
2025-07-05 20:50:51 +0100

    Fixing company migration

--- migrations/20240401102207_companies.php
+++ migrations/20240401102207_companies.php
@@ -6,7 +6,7 @@ use Phinx\Migration\AbstractMigration;
 
 final class Companies extends AbstractMigration
 {
-    public function up(): void
+    public function change(): void
     {
         $this->table('company_status')
             ->addColumn('name', 'string')
@@ -15,19 +15,21 @@ final class Companies extends AbstractMigration
             ->addColumn('archived', 'boolean')
             ->create();
 
-        $this->table('company_status')
-            ->insert([
-                ['name' => 'Pending', 'approved' => 0, 'pending' => 1, 'archived' => 0],
-                ['name' => 'Approved', 'approved' => 1, 'pending' => 0, 'archived' => 0],
-                ['name' => 'Archived', 'approved' => 0, 'pending' => 0, 'archived' => 1],
-            ])->update();
+        if ($this->isMigratingUp()) {
+            $this->table('company_status')
+                ->insert([
+                    ['name' => 'Pending', 'approved' => 0, 'pending' => 1, 'archived' => 0],
+                    ['name' => 'Approved', 'approved' => 1, 'pending' => 0, 'archived' => 0],
+                    ['name' => 'Archived', 'approved' => 0, 'pending' => 0, 'archived' => 1],
+                ])->saveData();
+        }
 
         $this->table('company')
             ->addColumn('date_created', 'datetime')
             ->addColumn('date_updated', 'datetime')
-            ->addColumn('status_id', 'integer')
+            ->addColumn('status_id', 'integer', ['signed' => false])
             ->addColumn('name', 'string')
-            ->addColumn('location_id', 'integer')
+            ->addColumn('location_id', 'integer', ['signed' => false])
             ->addColumn('phone_number', 'string', ['null' => true])
             ->addColumn('email_address', 'string', ['null' => true])
             ->addColumn('vat_number', 'string', ['null' => true])
@@ -36,20 +38,10 @@ final class Companies extends AbstractMigration
             ->create();
 
         $this->table('company_file', ['id' => false])
-            ->addColumn('company_id', 'integer')
-            ->addColumn('file_id', 'integer')
+            ->addColumn('company_id', 'integer', ['signed' => false])
+            ->addColumn('file_id', 'integer', ['signed' => false])
             ->addForeignKey('file_id', 'stored_file', 'id')
             ->addForeignKey('company_id', 'company', 'id')
-            ->create();;
-    }
-
-    public function down(): void
-    {
-        $this->table('company_file')
-            ->drop()->update();
-        $this->table('company')
-            ->drop()->update();
-        $this->table('company_status')
-            ->drop()->update();
+            ->create();
     }
 }
