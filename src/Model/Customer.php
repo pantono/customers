4ee01944a9c5737bc8af731fc6276aa702899368
Chris Burton <csburton@gmail.com>
2025-08-02 17:41:22 +0100

    Fixing lookup for user

--- src/Model/Customer.php
+++ src/Model/Customer.php
@@ -6,17 +6,17 @@ use Pantono\Authentication\Model\User;
 use Pantono\Contracts\Attributes\Locator;
 use Pantono\Customers\Customers;
 use Pantono\Contracts\Attributes\FieldName;
-use Pantono\Authentication\UserAuthentication;
 use Pantono\Contracts\Attributes\Lazy;
 use Pantono\Contracts\Attributes\NoSave;
 use Pantono\Core\Helper\ConfigHelper;
 use Pantono\Utilities\DateTimeParser;
+use Pantono\Authentication\Users;
 
 class Customer
 {
     private ?int $id = null;
     private \DateTimeInterface $dateCreated;
-    #[Locator(methodName: 'getUserById', className: UserAuthentication::class), FieldName('user_id'), Lazy]
+    #[Locator(methodName: 'getUserById', className: Users::class), FieldName('user_id'), Lazy]
     private ?User $user = null;
     #[Locator(methodName: 'getDetailsById', className: Customers::class), FieldName('details_id')]
     private ?CustomerDetail $details = null;
